bcae5c97dd37bff14015bd1c5b4c6693e39f8fe9
Soumitra Chatterjee <mrsoumitrachatterjee@gmail.com>
2025-04-12 18:42:47 +0530

    Add example input files; Cleanup old directory structure

--- retainium/commands/__init__.py
+++
@@ -1,8 +0,0 @@
-from retainium.add_knowledge import register_add_knowledge_command
-from retainium.list_knowledge import register_list_knowledge_command
-from retainium.query_knowledge import register_query_knowledge_command
-
-def register_all_commands(subparsers):
-    register_add_knowledge_command(subparsers)
-    register_list_knowledge_command(subparsers)
-    register_query_knowledge_command(subparsers)
